a55cae9a3b0e62ceb8753c9bb321720e13e61fc9
Jolly Yau <josephhu706@hotmail.com>
2022-06-12 00:04:37 +1000

    removed icon

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+
 
 
 export default function Navbar() {
@@ -20,7 +20,6 @@ export default function Navbar() {
                 aria-label="menu"
                 sx={{ mr: 2 }}
             >
-                <MenuIcon />
             </IconButton>
             <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                 News
