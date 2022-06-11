69080856d7db39fa4e3d4ebf6c7a92e1f6d100aa
Jolly Yau <josephhu706@hotmail.com>
2022-06-12 00:14:20 +1000

    deleted context

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import CryptoContext from './components/CryptoContext';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <CryptoContext>
       <App />
-    </CryptoContext>
   </React.StrictMode>
 );
 
