61640c0040a5457955e90b039acc6f0b6603720a
Jolly Yau <josephhu706@hotmail.com>
2022-06-13 00:18:43 +1000

    removed darkmode

--- src/pages/CoinPage.tsx
+++ src/pages/CoinPage.tsx
@@ -1,11 +1,4 @@
 import React from 'react'
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-
-const darkTheme = createTheme({
-  palette: {
-    mode: 'dark',
-  },
-});
 
 const CoinPage = () => {
   return (
