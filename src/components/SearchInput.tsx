33e2f7f125f06073704c479da304daa079d5d913
Jolly Yau <josephhu706@hotmail.com>
2022-06-15 22:39:59 +1000

    dynamic heading

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
 import logo from '../assets/images/logo.svg'
+import '../styles/SearchInputStyles.scss'
 
 type Props = {
     handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void
@@ -11,7 +12,7 @@ const SearchInput:React.FC<Props> = ({handleChange}) => {
   return (
     <div>
         <div data-testid='tableLogo'>
-        <img src={logo} style={{"height": "8em", "marginTop":"2em"}}  alt="logo"/>
+          <img src={logo} className="searchLogo" alt="logo"/>
             <Typography color="common.white" variant='h5' style={{margin:18, fontFamily:"Montserrat"}}>
                 Cook up Coins in the Crypto Cauldron
             </Typography>
